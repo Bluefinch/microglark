b37c7c8def6c35c7e4dfefac9d3373ebbdbceba3
Florent Galland <florent.galland@gmail.com>
2013-10-05 19:06:11 +0200

    Selection works

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-var sharejs = require('share')
-//var socketio = require('socket.io');
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var sharejs = require('share');
+var socketio = require('socket.io');
 
 var app = express();
 
@@ -21,25 +16,33 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('aerg68nu8itk6'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 
 var server = http.createServer(app);
-//var io = socketio.listen(server);
+var sio = socketio.listen(server);
 
 /* Attach the sharjs REST and Socket.io interfaces to the server. */
 var sharejsOptions = {db: {type: 'none'}};
 sharejs.server.attach(app, sharejsOptions);
     
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+sio.sockets.on('connection', function (socket) {
+    console.log('Websocket connection.');
+    socket.on('selectionChange', function (data) {
+        console.log(data);
+        socket.broadcast.emit('selectionChange', data);
+    });
 });
--- public/javascripts/microGlark.js
+++ public/javascripts/microGlark.js
@@ -1,29 +1,80 @@
-/* global ace, sharejs */
-var randomDocName = function (length) {
+/* global ace, sharejs, $, editor, socket, userId, documentId */
+var makeRandomHash = function (length) {
     var chars, x;
     if (!length) {
         length = 10;
     }
-    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-=";
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     var name = [];
     for (x = 0; x < length; x++) {
         name.push(chars[Math.floor(Math.random() * chars.length)]);
     }
     return name.join('');
 };
+
+var onSelectionChange = function () {
+    var selection = editor.getSelection().getRange();
+    socket.emit('selectionChange', {
+        userId: userId,
+        documentId: documentId,
+        selection: selection
+    });
+};
+
+var getSelectionMarkupForUser = function (userId) {
+    return '<div id="collaboration-selection-' + userId + '" class="collaboration-selection-wrapper">' +
+        '<div class="collaboration-selection"></div>' +
+        '<div class="collaboration-selection-tooltip">' + userId + '</div>' +
+        '</div>';
+};
+
+/* This function must be bound with the markup which contains
+ * the tooltip to hide. */
+var _hideTooltipAndRemoveAttrForBoundMarkup = function () {
+    this.children('.collaboration-selection-tooltip').fadeOut('fast');
+    this.removeAttr('hideTooltipTimeoutRef');
+};
+
+/* Show the tooltip of the given selection markup. If duration is defined, the
+ * tooltip is automaticaly hidden when the time is elapsed. */
+var showTooltipForMarkup = function (markup, duration) {
+    var timeoutRef = markup.attr('hideTooltipTimeoutRef');
+    if (timeoutRef !== undefined) {
+        clearTimeout(timeoutRef);
+        markup.removeAttr('hideTooltipTimeoutRef');
+    }
+
+    markup.children('.collaboration-selection-tooltip').fadeIn('fast');
+
+    if (duration !== undefined) {
+        timeoutRef = setTimeout(_hideTooltipAndRemoveAttrForBoundMarkup.bind(markup), duration);
+        markup.attr('hideTooltipTimeoutRef', timeoutRef);
+    }
+};
+
 window.onload = function () {
+    window.userId = makeRandomHash(5);
+
+    if (!document.location.hash) {
+        document.location.hash = '#' + makeRandomHash();
+    }
+
+    window.documentId = document.location.hash.slice(1);
+    var span = document.getElementById('documentId').innerText = documentId;
+
+    /* Initialize socket.io */
+    window.socket = io.connect();
+
+    /* Initialize ace. */
     var editor = window.editor = ace.edit("editor");
     editor.setReadOnly(true);
     editor.getSession().setUseSoftTabs(true);
     editor.getSession().setTabSize(2);
     editor.getSession().setMode("ace/mode/coffee");
     editor.setTheme("ace/theme/glarkio_black");
-    if (!document.location.hash) {
-        document.location.hash = '#' + randomDocName();
-    }
-    var docName = "code:" + document.location.hash.slice(1);
-    var span = document.getElementById('docname').innerText = docName;
-    sharejs.open(docName, 'text', function (error, doc) {
+
+    /* Initialize sharejs. */
+    sharejs.open(documentId, 'text', function (error, doc) {
         if (error) {
             console.error(error);
             return;
@@ -33,5 +84,59 @@ window.onload = function () {
         }
         doc.attach_ace(editor);
         editor.setReadOnly(false);
+
+        editor.getSelection().on('changeCursor', onSelectionChange);
+        editor.getSelection().on('changeSelection', onSelectionChange);
+    });
+
+    /* Bind some ui events. */
+    $(document).on('mouseenter',
+            '.collaboration-selection,.collaboration-selection-tooltip',
+            function () {
+                var markup = $(this).parent();
+                showTooltipForMarkup(markup);
+            });
+
+    $(document).on('mouseleave',
+            '.collaboration-selection,.collaboration-selection-tooltip',
+            function () {
+                var markup = $(this).parent();
+                showTooltipForMarkup(markup, 500);
+            });
+
+    /* Socket.io events. */
+    socket.on('selectionChange', function (data) {
+        if (data.documentId === documentId || true) {
+            console.log(data);
+            var screenCoordinates = editor.renderer
+            .textToScreenCoordinates(data.selection.start.row,
+                data.selection.start.column);
+            console.log(screenCoordinates);
+            /* Update the selection css to the correct position. */
+            var $selection = $('#collaboration-selection-' + data.userId);
+            if ($selection.length === 0) {
+                /* The markup for the selection of this user does not
+                 * exist yet. Append it to the dom. */
+                $selection = $(getSelectionMarkupForUser(data.userId));
+                $('body').append($selection);
+            }
+
+            /* Check if the selection has changed. */
+            if ($selection.css('left').slice(0, -2) !== String(screenCoordinates.pageX) ||
+                $selection.css('top').slice(0, -2) !== String(screenCoordinates.pageY)) {
+
+                $selection.css({
+                    left: screenCoordinates.pageX,
+                    top: screenCoordinates.pageY
+                });
+
+                // $selection.children('.collaboration-selection')
+                    // .css('background-color', selections[username].color);
+                // $selection.children('.collaboration-selection-tooltip')
+                    // .css('background-color', selections[username].color);
+
+                showTooltipForMarkup($selection, 2000);
+            }
+        }
     });
 };
