6f41220917c9d372d2c4738e6db6a0aa9669f71c
Verdier <luc.verdier1@gmail.com>
2013-10-06 11:39:51 +0200

    Remove user

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var sharejs = require('share');
