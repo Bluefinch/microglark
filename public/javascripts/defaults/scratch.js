c9f98496a7e9d925fbb1c2709651a1ccbabc4cff
Florent Galland <florent.galland@gmail.com>
2013-10-07 10:45:47 +0200

    Add line break in scratch.js

--- public/javascripts/defaults/scratch.js
+++ public/javascripts/defaults/scratch.js
@@ -2,7 +2,7 @@ window.defaultFile = {
     filename: 'Scratch.js',
     content: "/* µglark.io is a minimalistic pair programing editor.\n" +
         " * Share the url of this page " + window.location.toString() +
-        " with anybody, and start collaborating editing.\n" +
+        " with anybody,\n *  and start collaborating editing.\n" +
         " * Drag and drop any file from your desktop here to open it.\n" +
         " * Enjoy! */\n" +
         " exports.glark = function () {\n    console.log('hi!');\n};"
