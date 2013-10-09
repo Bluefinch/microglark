746a51381a1f246022aba0df460e5153c43d7951
Flolagale <florent.galland@gmail.com>
2013-10-09 14:45:16 +0200

    Update scratch.js

--- public/javascripts/defaults/scratch.js
+++ public/javascripts/defaults/scratch.js
@@ -5,6 +5,7 @@ window.defaultFile = {
         " * Share the url of this page " + window.location.toString() +
         " with anybody,\n * and start collaborative editing.\n" +
         " * Drag and drop any file from your desktop here to open it.\n" +
+        " * Edit the filename just above the editor to change the syntax highlighting type." +
         " * Enjoy! */\n" +
         "exports.glark = function () {\n    console.log('hi!');\n};";
     }
