66cae65780c501fd1298ca591e2435e9a2c6135d
Verdier <luc.verdier1@gmail.com>
2013-10-09 10:24:01 +0200

    Fix typo

--- public/javascripts/defaults/scratch.js
+++ public/javascripts/defaults/scratch.js
@@ -3,7 +3,7 @@ window.defaultFile = {
     getContent: function () {
         return "/* µGlark.io is a minimalistic pair programing editor.\n" +
         " * Share the url of this page " + window.location.toString() +
-        " with anybody,\n * and start collaborating editing.\n" +
+        " with anybody,\n * and start collaborative editing.\n" +
         " * Drag and drop any file from your desktop here to open it.\n" +
         " * Enjoy! */\n" +
         "exports.glark = function () {\n    console.log('hi!');\n};";
