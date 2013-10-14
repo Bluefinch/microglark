53e39bb7acd041d34f34a28670d4ac1203a35144
Flolagale <florent.galland@gmail.com>
2013-10-14 09:56:36 +0200

    Update scratch.js

--- public/javascripts/defaults/scratch.js
+++ public/javascripts/defaults/scratch.js
@@ -1,11 +1,13 @@
 window.defaultFile = {
     filename: 'Scratch.js',
     getContent: function () {
-        return "/* µGlark.io is a minimalistic pair programing editor.\n" +
-        " * Share the url of this page " + window.location.toString() +
-        " with anybody,\n * and start collaborative editing.\n" +
+        return "/* µGlark.io is a minimalistic pair programming editor.\n" +
+        " * Share the url of this page " + window.location.toString() + " with anybody,\n" +
+        " * and start collaborative editing.\n" +
         " * Drag and drop any file from your desktop here to open it.\n" +
         " * Edit the filename just above the editor to change the syntax highlighting type.\n" +
+        " * Because we value privacy, nothing is stored on the server,\n" +
+        " * closing your browser tab will instantly destroy the document.\n" +
         " * Enjoy! */\n" +
         "exports.glark = function () {\n    console.log('hi!');\n};";
     }
